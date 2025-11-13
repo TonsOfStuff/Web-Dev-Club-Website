b2a503e6e45248b124c98067973794629c4f6bfa
TonsOfStuff <jtons2008@gmail.com>
2025-11-13 16:26:45 -0600

    n

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ window.addEventListener('scroll', function () {
     header.classList.add('scrolled');
     document.getElementById("titleImg").classList.add("imgScrolled")
     document.getElementById("titleImg").classList.remove("starterImg")
+
+    document.querySelector("article").style.display = "block"
+    document.getElementById("filler").style.display = "none"
   } else {
     header.classList.remove('scrolled');
     document.getElementById("titleImg").classList.remove("imgScrolled")
     document.getElementById("titleImg").classList.add("starterImg")
+
+    document.querySelector("article").style.display = "none"
+    document.getElementById("filler").style.display = "block"
   }
 });
