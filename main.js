8e12c3cdc0ad1fb6915df9ac947441e602b0b78b
Jerry Du <80072621+TonsOfStuff@users.noreply.github.com>
2026-02-26 16:25:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('mainHeader');
+  if (window.screen.width > 848){
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+      document.getElementById("titleImg").classList.add("imgScrolled")
+      document.getElementById("titleImg").classList.remove("starterImg")
 
-        <link rel = "stylesheet" href = "styles.css">
-        <script defer src="main.js"></script>
+      document.querySelector("article").style.display = "block"
+      document.getElementById("filler").style.display = "none"
+    } else {
+      header.classList.remove('scrolled');
+      document.getElementById("titleImg").classList.remove("imgScrolled")
+      document.getElementById("titleImg").classList.add("starterImg")
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Cascadia+Code:ital,wght@0,200..700;1,200..700&display=swap" rel="stylesheet">
+      document.querySelector("article").style.display = "none"
+      document.getElementById("filler").style.display = "block"
 
-        <title>Web Dev Club</title>
-    </head>
-    <body>
-        <header id = "mainHeader">
-            <div id = "Logo">Web Dev Club</div>
-            <nav>
-                <a href = "index.html">Home</a>
-                <a href = "aboutUs.html">About Us</a>
-                <a href = "contact.html">Contact</a>
-            </nav>
-        </header>
+      const chunks = document.querySelectorAll(".chunk");
+      chunks.forEach((element) => element.classList.remove("shownItem"));
+    }
+  }
+  
+});
 
-        <main>
-            <img id = "titleImg" class = "starterImg" src="https://files.123freevectors.com/wp-content/original/154304-bright-blue-background-vector-image.jpg">
-            <article>
-                <div id = "story1" class = "chunk">
-                    <h2>Ever wanted to learn how to make websites? <p>Join us at New Trier's Web Dev Club to learn how to code, design, and create your very own website!</p></h2>
-                    <img id = "storyImg" src="https://web.dev/static/images/learn-header.png">
-                </div>
-                <div id = "story2" class = "chunk">
-                    <img id = "storyImg" src="https://images.businessnewsdaily.com/app/uploads/2022/04/04074130/teamwork_g-stockstudio_getty.jpg">
-                    <h2>No previous experience required. <p>Learn how to use HTML and CSS or build websites with personalized website builders.</p></h2>
-                </div>
-                <div id = "story3" class = "chunk">
-                    <h1>Interested in us making a website for you?</h1>
-                    <p>Take this quick form to let us know!</p>
-                    <div id ="buttonContainer">
-                        <button type = "button" id = "formButton">Click here!</button>
-                    </div>
-                </div>
-            </article>
-            <div id = "filler" style="min-height: 1000px;"></div>
-        </main>
 
-        <footer>
-            <div class = "footerSub">
-                <a href="index.html">Home</a>
-                <a href="aboutUs.html">About Us</a>
-                <a href="contact.html">Contact Us</a>
-            </div>
-            <div class = "footerSub">
-                <div>New Trier Township High School</div>
-                <div>Thursdays 3:30-4:30</div>
-                <div>Room 124 Winnetka Campus</div>
-            </div>
-        </footer>
-    </body>
-</html>
+const formButton = document.getElementById("formButton");
+formButton.addEventListener("click", () => {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfN_CXZKR1Fs__wsTfVA0vrhpv4F2Inl8bFhBiZOYWfZpnG9A/viewform?usp=header";
+})
+
+//Scroll and show item code
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add("shownItem");
+      }
+  })
+});
+const chunks = document.querySelectorAll(".chunk");
+chunks.forEach((element) => observer.observe(element));
